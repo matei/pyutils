dd9222c92d6e971a111d22df54af20292cbf7d10
Matei Stefanescu <mstefanescu@optaros.com>
2022-11-03 22:18:07 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup (
+    name="pyutils",
+    version="1.0",
+    url="https://github.com/matei/pyutils",
+    author="Matei Stefanescu",
+    py_modules=["db_utils","defaults","kube","net_utils","notify","op_exception","parallel_work","print_utils","progress_bar","static_config","wait"]
+)
